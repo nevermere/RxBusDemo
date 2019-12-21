0a58aec9f5f1abfec669e1efa36c306bf3fbcb27
江凤洲 <nevermere@163.com>
2019-12-21 09:36:04 +0800

    DoraemonKit

--- app/src/main/java/com/hz/rxbus/demo/App.java
+++ app/src/main/java/com/hz/rxbus/demo/App.java
@@ -15,6 +15,8 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // 初始化DoraemonKit
         DoraemonKit.install(this);
     }
 }
